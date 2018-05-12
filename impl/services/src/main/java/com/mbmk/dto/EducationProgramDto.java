09f8ac2ae41115365affe42d93e275c958328566
mariuszbancer <mariusz.bancer@gmail.com>
2018-05-12 19:12:31 +0200

    Setting up project

---
+++ impl/services/src/main/java/com/mbmk/dto/EducationProgramDto.java
@@ -0,0 +1,8 @@
+package com.mbmk.dto;
+
+import lombok.Data;
+
+@Data
+public class EducationProgramDto {
+
+}
---
+++ impl/services/src/main/java/com/mbmk/mappers/SubjectMapper.java
@@ -0,0 +1,8 @@
+package com.mbmk.mappers;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SubjectMapper {
+
+}
